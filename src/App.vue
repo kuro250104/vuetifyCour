a6b75ca4f427b7044a0d3b7b2707679784e03c82
Kuro <45374906+kuro250104@users.noreply.github.com>
2024-03-26 10:48:20 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-btn density="default" icon="mdi-open-in-new"></v-btn>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  //
+</script>
+
+<style>
+
+</style>
